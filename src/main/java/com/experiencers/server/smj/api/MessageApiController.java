1153ace8a6c941950c6173ec43d7cd250f47ce57
Dooo-Beee <qkrdbstn25@gmail.com>
2021-01-29 12:08:59 +0900

    MemberApiController, MessageApiController

---
+++ src/main/java/com/experiencers/server/smj/api/MessageApiController.java
@@ -0,0 +1,60 @@
+package com.experiencers.server.smj.api;
+
+
+import com.experiencers.server.smj.domain.Message;
+import com.experiencers.server.smj.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageApiController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping("")
+    public List<Message> getMessages(){
+        List<Message> messageList = messageService.readAllMessage();
+
+        return messageList;
+    }
+
+    @PostMapping("")
+    public Message postMessage(@RequestBody Message message, @PathVariable("member_id")Long memberId){
+        Message savedMessage = messageService.saveMessage(message,memberId);
+
+        return savedMessage;
+    }
+
+    @PutMapping("/{message_id}")
+    public ResponseEntity<Message> putMessage(@PathVariable("message_id")Long messageId, @RequestBody Message message){
+        Message updatedMessage = messageService.readAndUpdateMessage(messageId, message);
+
+        if(updatedMessage == null){
+            message.setMessage_id(messageId);
+
+            return new ResponseEntity<>(updatedMessage, HttpStatus.NOT_FOUND);
+
+        }
+        return new ResponseEntity<>(updatedMessage, HttpStatus.OK);
+    }
+    @DeleteMapping("/{message_id}")
+    public ResponseEntity<Object> deleteMessage(@PathVariable("message_id")Long messageId){
+        messageService.deleteMessage(messageId);
+
+        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("message_id", messageId);
+        result.put("message", data);
+
+        return new ResponseEntity<>(result,HttpStatus.OK);
+    }
+}
